7b09f183bf4f1f46c19259dc15408c59ed72121c
shobhavj <shobha.vj@gmail.com>
2017-09-09 17:56:14 -0400

    Initial Commit

---
+++ TrialForGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrialForGit
+//
+//  Created by Shobha V J on 9/9/2017.
+//  Copyright © 2017 Shobha V J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
