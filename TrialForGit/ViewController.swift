5b5c6314c73082089e0eeff00787d7f0f29865da
shobhavj <shobha.vj@gmail.com>
2017-09-09 21:36:27 -0400

    print again

--- TrialForGit/ViewController.swift
+++ TrialForGit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("I cant wait ....")
         print("I am happy")
+         print("I am excited")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
