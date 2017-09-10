3e5898a379dfb679ec2f2fa1bac4958269d82158
shobhavj <shobha.vj@gmail.com>
2017-09-09 21:18:31 -0400

    second print

--- TrialForGit/ViewController.swift
+++ TrialForGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("I cant wait ....")
+        print("I am happy")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
